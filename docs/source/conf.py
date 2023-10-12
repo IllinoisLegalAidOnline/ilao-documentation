abd3eef05f4a026221e6d907e24307c108615255
Gwendelyn Daniels <gwendelyndaniels@GwendelynsMBP2.ad.lafchicago.org>
2023-10-12 14:11:04 -0500

    Make clean

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,20 +93,7 @@ html_context = {
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_theme_options = {
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+
 
 
 
