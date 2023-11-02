a269b9e23fc7b73e599dcda7dc2a5dd1377fe5a0
Gwendelyn Daniels <gwendelyndaniels@GwendelynsMBP2.attlocal.net>
2023-11-02 11:50:24 -0500

    Updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = 'Sprint 124a'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo',
-    'sphinx.ext.imgmath'
+    'sphinx.ext.imgmath',
+    'sphinx_rtd_theme'
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,8 +56,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'press'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'press'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
